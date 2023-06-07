e183d33e8483453b59e51e90f2c07ae184870482
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 06:37:31 +0200

    ti

--- app/src/main/java/com/example/timernetic/MainActivity.kt
+++ app/src/main/java/com/example/timernetic/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.example.timernetic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.withimage.databinding.ActivityNavdrBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNavdrBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
