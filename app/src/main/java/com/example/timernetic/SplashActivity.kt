53e728def95d8afc1d42d18625760e43c14676f9
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 06:00:35 +0200

    ti

--- app/src/main/java/com/example/timernetic/SplashActivity.kt
+++ app/src/main/java/com/example/timernetic/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.example.timernetic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 2000 // Delay in milliseconds
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        // Delay the execution of the next activity
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
     }
 }
