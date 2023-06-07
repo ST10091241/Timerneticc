7901b661a91403b3c1a8e95ec3a8f3724e1a2c85
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 07:06:56 +0200

    ti

--- app/src/main/java/com/example/timernetic/MainActivity.kt
+++ app/src/main/java/com/example/timernetic/MainActivity.kt
@@ -1,23 +1,66 @@
 package com.example.timernetic
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.withimage.databinding.ActivityNavdrBinding
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityNavdrBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener { item ->
+            // Handle navigation item clicks here
+            when (item.itemId) {
+                R.id.nav_option1 -> {
+                    // option 1 click
+                    // val navController = findNavController(this, R.id.nav_host_fragment)
+                    // navController.navigate(R.id.action_splashFragment_to_homeFragment)
+                }
+                R.id.nav_option2 -> {
+                    val intent = Intent(this, Dailygoal::class.java)
+                    startActivity(intent)
+                }
+
+
+                R.id.nav_option3 -> {
+                    // Handle option 3 click
+                    val intent = Intent(this, TimesheetView::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_option4 -> {
+                    // Handle option 4 click
+
+                }
+                R.id.nav_option5 -> {
+                    // Handle option 5 click
+                    val intent = Intent(this, MainMenu::class.java)
+                    startActivity(intent)
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
