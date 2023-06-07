80a8d466b58fe8b080d7c3406c6a9e4e7a566acf
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 06:33:52 +0200

    ti

--- app/src/main/java/com/example/timernetic/LoginActivity.kt
+++ app/src/main/java/com/example/timernetic/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.timernetic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -20,5 +23,24 @@ class LoginActivity : AppCompatActivity() {
         emailtxt = findViewById(R.id.emailtxt)
         passwordtxt = findViewById(R.id.passwordtxt)
 
+       proceedbtn.setOnClickListener {
+            val email =emailtxt.text.toString().trim()
+            val pass = passwordtxt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(
+                    OnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(applicationContext, "Logged In Successfully", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(applicationContext, it.exception?.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/timernetic/SignupActivity.kt
+++ app/src/main/java/com/example/timernetic/SignupActivity.kt
@@ -1,8 +1,12 @@
 package com.example.timernetic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
@@ -10,15 +14,36 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var proceedbtn:ImageView
     private lateinit var emailtxt: EditText
     private lateinit var passwordtxt: EditText
-    private lateinit var confirmPasswordcrd: EditText
+    private lateinit var confirmPasswordtxt: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         proceedbtn = findViewById(R.id.proceedbtn)
         emailtxt = findViewById(R.id.emailtxt)
         passwordtxt = findViewById(R.id.passwordtxt)
-        confirmPasswordcrd = findViewById(R.id.confirmPasswordcrd)
-        proceedbtn = findViewById(R.id.proceedbtn)
-        proceedbtn = findViewById(R.id.proceedbtn)
+        confirmPasswordtxt = findViewById(R.id.confirmPasswordtxt)
+
+        proceedbtn.setOnClickListener{
+            val email = emailtxt.text.toString().trim()
+            val pass = passwordtxt.text.toString().trim()
+            val verifyPass = confirmPasswordtxt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()){
+                if(pass == verifyPass){
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if(it.isSuccessful){
+                                Toast.makeText(applicationContext,"Registered Successfully", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@SignupActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }else{
+                                Toast.makeText(applicationContext, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    )
+                }
+            }
+        }
     }
 }
