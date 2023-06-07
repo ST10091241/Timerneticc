a1405b1d6b5bf49c91043d3db5427d6da921a7c6
ST10091241 <ST10091241@VCCONNECT.EDU.ZA>
2023-06-07 05:46:43 +0200

    ti

---
+++ app/src/main/java/com/example/timernetic/ViewTimesheetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timernetic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTimesheetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_timesheet)
+    }
+}
